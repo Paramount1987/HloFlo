f693514338774b6f51d467f22f55b51fb6c71c55
Artem Petrov <paramount7891@gmail.com>
2022-01-29 18:47:26 +0300

    backup main page

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,38 @@
 // // Import vendor jQuery plugin example
 // import '~/app/libs/mmenu/dist/mmenu.js'
+import Swiper from "swiper/bundle";
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+	const swiperReview = new Swiper(".swiper-review", {
+		slidesPerView: "auto",
+		spaceBetween: 30,
+		loop: true,
 
-	// Custom JS
+		autoplay: {
+			delay: 2500,
+			// disableOnInteraction: false,
+		},
 
-})
+		// Navigation arrows
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev",
+		},
+	});
+
+	const swiperGame = new Swiper(".swiper-game", {
+		slidesPerView: 1,
+		spaceBetween: 30,
+		loop: true,
+
+		// autoplay: {
+		// 	delay: 2500,
+		// },
+
+		// Navigation arrows
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev",
+		},
+	});
+});
